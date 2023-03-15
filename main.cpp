1fae0d2753f1252d38e57248abc1c7a88e604139
Csaba Sipos <siposcsaba89@gmail.com>
2023-03-15 12:42:45 +0100

    socket echo client test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,75 @@
 #include<GLFW/glfw3.h>
 #include <iostream>
 
+
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int testSocket() {
+	const char* server_name = "127.0.0.1";
+	const int server_port = 65431;
+
+	struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+
+	// creates binary representation of server name
+	// and stores it as sin_addr
+	// http://beej.us/guide/bgnet/output/html/multipage/inet_ntopman.html
+	inet_pton(AF_INET, server_name, &server_address.sin_addr);
+
+	// htons: port in network order format
+	server_address.sin_port = htons(server_port);
+
+	// open a stream socket
+	int sock;
+	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("could not create socket %d \n", sock);
+		return 1;
+	}
+
+	// TCP is connection oriented, a reliable connection
+	// **must** be established before any data is exchanged
+    int res = connect(sock, (struct sockaddr*)&server_address,
+	            sizeof(server_address));
+	if ( res < 0) {
+		printf("could not connect to server %d\n", res);
+		//return 1;
+	}
+
+	// send
+
+	// data that will be sent to the server
+	const char* data_to_send = "Gangadhar Hi Shaktimaan hai";
+	send(sock, data_to_send, strlen(data_to_send), 0);
+
+	// receive
+
+	int n = 0;
+	int len = 0, maxlen = 100;
+	char buffer[maxlen];
+	char* pbuffer = buffer;
+
+	// will remain open until the server terminates the connection
+	while ((n = recv(sock, pbuffer, maxlen, 0)) > 0) {
+		pbuffer += n;
+		maxlen -= n;
+		len += n;
+
+		buffer[len] = '\0';
+		printf("received: '%s'\n", buffer);
+	}
+
+	// close the socket
+	close(sock);
+	return 0;
+}
+
+
 int windowWidth;
 int windowHeight;
 
@@ -43,6 +112,8 @@ void do_frame(){
 
 int main(int argc, char **argv) {
 
+
+testSocket();
 
 if (glfwInit()!=GL_TRUE) {
     printf("glfwInit() failed\n");
